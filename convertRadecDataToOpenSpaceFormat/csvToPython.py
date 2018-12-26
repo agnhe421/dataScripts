385fda6aa90e2656173f3ee2280ac74e57489331
Agnes Heppich <agnhe421@student.liu.se>
2018-12-26 16:16:43 -0500

    clean up

--- convertRadecDataToOpenSpaceFormat/csvToPython.py
+++ convertRadecDataToOpenSpaceFormat/csvToPython.py
@@ -10,7 +10,7 @@ data = []
 oneHourData = []
 
 
-with open('data/MRO.dat') as f:
+with open('data/STB.dat') as f:
 	reader = csv.DictReader(f, fieldnames=['TimeStamp', 'AzUp', 'ElUp', 'RngUp', 'AzDn', 'ElDn', 'RngDn', 
 		'RAUp', 'DecUp', 'GeoRngUp', 'RADn', 'DecDn', 'GeoRngDn', 'ULT', 'RTLT_Up', 'XADop', 'DLT', 'RTLT_Dn', 'OneWayDop', 'TwoWayDop']) 
 	for row in reader:
@@ -20,9 +20,9 @@ with open('data/MRO.dat') as f:
 		del row['AzDn']
 		del row['ElDn']
 		del row['RngDn']
-		del row['RADn'] 
-		del row['DecDn']
-		del row['GeoRngDn']
+		del row['RAUp'] 
+		del row['DecUp']
+		del row['GeoRngUp']
 		del row['ULT']
 		del row['RTLT_Up']
 		del row['XADop']
@@ -31,9 +31,10 @@ with open('data/MRO.dat') as f:
 		del row['TwoWayDop']
 		del row['RTLT_Dn']
 
-		# row['RAUp'] = float(row['RAUp'])
-		# row['DecUp'] = float(row['DecUp'])
-		# row['GeoRngUp'] = float(row['GeoRngUp'])
+		row['RADn'] = float(row['RADn'])
+		row['DecDn'] = float(row['DecDn'])
+		row['GeoRngDn'] = float(row['GeoRngDn'])
+		row['DLT'] = float(row['DLT'])
 		row['TimeStamp'] = row['TimeStamp'][0:14]
 		
 		name = row["TimeStamp"][0:11]
@@ -48,7 +49,7 @@ for i in range(0, len(hours)):
 	if(i+1 < len(hours) and hours[i] == hours[i+1]):
 		oneHourData.append(data[i])
 	else:
-		outputData = open(str("C:/Users/openspace/Agnes/convertToJSON/convertRadecDataToOpenSpaceFormat/parsed/MRO/" + names[i])+'.json', 'w')
+		outputData = open(str("C:/Users/openspace/Agnes/convertToJSON/convertRadecDataToOpenSpaceFormat/parsed/STB/" + names[i])+'.json', 'w')
 		positions["Positions"] = oneHourData
 		json.dump(positions,outputData, indent=4)
 		del oneHourData[:]
