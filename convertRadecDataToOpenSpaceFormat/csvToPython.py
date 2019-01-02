581eef017fe08f52665f075cbcce63a5751d9c13
Agnes Heppich <agnhe421@student.liu.se>
2019-01-02 12:38:11 -0500

    made some generalisations, such as specifying local path

--- convertRadecDataToOpenSpaceFormat/csvToPython.py
+++ convertRadecDataToOpenSpaceFormat/csvToPython.py
@@ -1,16 +1,28 @@
 import csv
 import json
 from collections import defaultdict
+from pathlib import Path
 
-name = ""
+
+localPath = "C:/Users/openspace/Agnes/convertToJSON/convertRadecDataToOpenSpaceFormat/"
+generatedDataDir = "parsed/"
+generatedExtension = ".json"
+spacecraftName = "NHPC_TEST"
+dataDir = "data/"
+dataExtension = ".dat"
+
+# Generated filename as date, Format: YYYY-DDDTHH
+nameFormat = ""
 hours = []
-names = []
+nameFormatList = []
 positions = {}
 data = []
 oneHourData = []
 
 
-with open('data/STB.dat') as f:
+dataPath = dataDir + spacecraftName + dataExtension
+
+with open(dataPath) as f:
 	reader = csv.DictReader(f, fieldnames=['TimeStamp', 'AzUp', 'ElUp', 'RngUp', 'AzDn', 'ElDn', 'RngDn', 
 		'RAUp', 'DecUp', 'GeoRngUp', 'RADn', 'DecDn', 'GeoRngDn', 'ULT', 'RTLT_Up', 'XADop', 'DLT', 'RTLT_Dn', 'OneWayDop', 'TwoWayDop']) 
 	for row in reader:
@@ -31,25 +43,33 @@ with open('data/STB.dat') as f:
 		del row['TwoWayDop']
 		del row['RTLT_Dn']
 
+		#print(row['TimeStamp'])
 		row['RADn'] = float(row['RADn'])
 		row['DecDn'] = float(row['DecDn'])
 		row['GeoRngDn'] = float(row['GeoRngDn'])
 		row['DLT'] = float(row['DLT'])
 		row['TimeStamp'] = row['TimeStamp'][0:14]
 		
-		name = row["TimeStamp"][0:11]
-		names.append(name)
-
+		#Create our filename from the timestamp
+		nameFormat = row["TimeStamp"][0:11]
+		nameFormatList.append(nameFormat)
+		#print(name)
 		hour = row["TimeStamp"][9:11]
 		hours.append(int(hour))
 
 		data.append(row)
 
-for i in range(0, len(hours)):
-	if(i+1 < len(hours) and hours[i] == hours[i+1]):
+for i in range(0, len(data)):
+	if(i+1 < len(nameFormatList) and nameFormatList[i] == nameFormatList[i+1]):
 		oneHourData.append(data[i])
-	else:
-		outputData = open(str("C:/Users/openspace/Agnes/convertToJSON/convertRadecDataToOpenSpaceFormat/parsed/STB/" + names[i])+'.json', 'w')
+	elif (len(oneHourData) > 0):
+
+		fileNameString = str( localPath + generatedDataDir + spacecraftName + "/" + names[i] + generatedExtension )
+		my_file = Path(fileNameString)
+		if my_file.is_file():
+			#print("File already exists for" + fileNameString)
+
+		outputData = open(fileNameString, 'w')
 		positions["Positions"] = oneHourData
 		json.dump(positions,outputData, indent=4)
 		del oneHourData[:]
